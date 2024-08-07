82ebf14b264967740856c6e4f6c86630b06fd2f2
AnasMouak <anasmouak1111@gmail.com>
2024-08-07 19:10:42 +0000

    Add bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -6,14 +6,23 @@ DELIMITER //
 
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-    -- Insert the project if it doesn't exist and get the project id
-    INSERT INTO projects (name) 
-    VALUES (project_name) 
-    ON DUPLICATE KEY UPDATE id = LAST_INSERT_ID(id);
-    
+    DECLARE project_id INT;
+
+    -- Try to find the project ID, or insert a new project if it doesn't exist
+    SELECT id INTO project_id
+    FROM projects
+    WHERE name = project_name
+    LIMIT 1;
+
+    IF project_id IS NULL THEN
+        -- Insert new project and get the new project ID
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+
     -- Insert the correction
     INSERT INTO corrections (user_id, project_id, score)
-    VALUES (user_id, LAST_INSERT_ID(), score);
+    VALUES (user_id, project_id, score);
 END //
 
 DELIMITER ;
