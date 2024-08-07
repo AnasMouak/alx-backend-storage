5278b8d1fb9a21357a2a1ead2852e9e4602ab725
AnasMouak <anasmouak1111@gmail.com>
2024-08-07 13:45:29 +0000

    country_users

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- This script creates a 'users' table with id, email, name, and country attributes
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
